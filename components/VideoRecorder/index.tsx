c6d857dba7070f025f927dd4ea257cf9abf1a347
Alisson Martineli <martineli.alisson@gmail.com>
2021-10-20 13:14:58 -0300

    Added upload

--- components/VideoRecorder/index.tsx
+++ components/VideoRecorder/index.tsx
@@ -1,9 +1,15 @@
-import { Button, Stack } from '@material-ui/core'
+import { Button, Snackbar, Stack } from '@material-ui/core'
 import { FunctionComponent, useEffect, useRef, useState } from 'react'
 // eslint-disable-next-line no-unused-vars
 import adapter from 'webrtc-adapter'
+import axios from 'axios'
 
 const constraints = { video: true, audio: true }
+
+type ResponseType = {
+  responseTime: number
+  filesS3: string
+}
 
 const VideoCapture: FunctionComponent = () => {
   const video = useRef<HTMLVideoElement>(null)
@@ -13,6 +19,8 @@ const VideoCapture: FunctionComponent = () => {
   )
   const [chunks, setChunks] = useState<any[]>([])
   const [recording, setRecording] = useState(false)
+  const [notification, setNotification] = useState(false)
+  const [url, setUrl] = useState('')
 
   useEffect(() => {
     navigator.mediaDevices
@@ -64,43 +72,104 @@ const VideoCapture: FunctionComponent = () => {
     window.URL.revokeObjectURL(url)
   }
 
-  return (
-    <Stack alignItems="center" sx={{ position: 'relative' }}>
-      <video
-        ref={video}
-        autoPlay
-        muted
-        playsInline
-        style={{
-          maxHeight: '100vh',
-          maxWidth: '100vw',
-          transform: 'scale(-1, 1)',
-          WebkitTransform: 'scale(-1, 1)'
-        }}
-      />
+  const upload = () => {
+    const blob = new Blob(chunks, {
+      type: 'video/mp4'
+    })
 
-      <Stack
-        spacing={2}
-        direction="row"
-        justifyContent="center"
-        sx={{ position: 'absolute', bottom: 0 }}
-        p={4}
-      >
-        {!recording && !chunks.length ? (
-          <Button variant="contained" color="primary" onClick={startRecording}>
-            Start Recording
-          </Button>
-        ) : chunks.length ? (
-          <Button variant="contained" color="primary" onClick={download}>
-            Download
-          </Button>
-        ) : (
-          <Button variant="contained" color="secondary" onClick={stopRecording}>
-            Stop Recording
-          </Button>
-        )}
+    const data = new FormData()
+    data.append('file', blob, 'react-video-capture.mp4')
+
+    const headers = new Headers()
+    headers.append('Accept', 'application/json')
+
+    axios({
+      method: 'POST',
+      url: '/upload',
+      data
+    })
+      .then(response => {
+        const data = response.data as ResponseType
+        setUrl(data.filesS3)
+        setNotification(true)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+
+  const action = (
+    <Button
+      color="secondary"
+      size="small"
+      onClick={() => {
+        window.open(url)
+      }}
+    >
+      VER
+    </Button>
+  )
+
+  return (
+    <>
+      <Stack alignItems="center" sx={{ position: 'relative' }}>
+        <video
+          ref={video}
+          autoPlay
+          muted
+          playsInline
+          style={{
+            maxHeight: '100vh',
+            maxWidth: '100vw',
+            transform: 'scale(-1, 1)',
+            WebkitTransform: 'scale(-1, 1)'
+          }}
+        />
+
+        <Stack
+          spacing={2}
+          direction="row"
+          justifyContent="center"
+          sx={{ position: 'absolute', bottom: 0 }}
+          p={4}
+        >
+          {!recording && !chunks.length ? (
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={startRecording}
+            >
+              Start Recording
+            </Button>
+          ) : chunks.length ? (
+            <>
+              <Button variant="contained" color="primary" onClick={download}>
+                Download
+              </Button>
+              <Button variant="contained" color="secondary" onClick={upload}>
+                Enviar
+              </Button>
+            </>
+          ) : (
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={stopRecording}
+            >
+              Stop Recording
+            </Button>
+          )}
+        </Stack>
       </Stack>
-    </Stack>
+
+      <Snackbar
+        open={notification}
+        autoHideDuration={6000}
+        onClose={() => setNotification(false)}
+        message="Vídeo enviado com sucesso!"
+        action={action}
+      />
+    </>
   )
 }
 
